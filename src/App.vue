e1850d9feb5304464bd2468c161a4d2fb61bfb1a
KiHanLee <s3238382606@163.com>
2021-12-28 17:13:07 +0800

    Modify time font and add weather component.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
-  <img :src="`./wallpaper/home-${data.background}.jpg`" alt="bg">
+  <img :src="`./wallpaper/home-${data.background}.jpg`" alt="bg" />
   <!-- 右上角帮助按钮 -->
-  <svg class="help-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" @click="background.help = true">
-    <path d="M12 6a3.939 3.939 0 0 0-3.934 3.934h2C10.066 8.867 10.934 8 12 8s1.934.867 1.934 1.934c0 .598-.481 1.032-1.216 1.626a9.208 9.208 0 0 0-.691.599c-.998.997-1.027 2.056-1.027 2.174V15h2l-.001-.633c.001-.016.033-.386.441-.793.15-.15.339-.3.535-.458.779-.631 1.958-1.584 1.958-3.182A3.937 3.937 0 0 0 12 6zm-1 10h2v2h-2z"></path>
-    <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path>
+  <svg
+    class="help-icon"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    @click="background.help = true"
+  >
+    <path
+      d="M12 6a3.939 3.939 0 0 0-3.934 3.934h2C10.066 8.867 10.934 8 12 8s1.934.867 1.934 1.934c0 .598-.481 1.032-1.216 1.626a9.208 9.208 0 0 0-.691.599c-.998.997-1.027 2.056-1.027 2.174V15h2l-.001-.633c.001-.016.033-.386.441-.793.15-.15.339-.3.535-.458.779-.631 1.958-1.584 1.958-3.182A3.937 3.937 0 0 0 12 6zm-1 10h2v2h-2z"
+    ></path>
+    <path
+      d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"
+    ></path>
   </svg>
   <!-- 帮助 -->
   <div class="help" v-show="data.help" :class="{ 'help-on': data.help }">
     <div class="help-content">
       <div>
         <h2>快捷键解释</h2>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" @click="data.help = false">
-          <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          @click="data.help = false"
+        >
+          <path
+            d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"
+          ></path>
         </svg>
       </div>
       <ul>
@@ -22,29 +41,41 @@
       </ul>
     </div>
   </div>
+
+  <!-- 天气组件 -->
+  <vWeather />
+
   <!-- 搜索栏 -->
   <div class="search-box">
-    <Clock />
+    <vClock />
     <br />
-    <Input placeholder="输入搜索内容" @updateEvent="inputUpdateEvent" />
+    <vInput placeholder="输入搜索内容" @updateEvent="inputUpdateEvent" />
   </div>
+
   <!-- 装饰用的横线 -->
   <hr v-show="data.result.length && data.keywords.length" />
+
   <!-- 关键词联想列表 -->
-  <List :listData="data.result" :selected="data.selected" :keywords="data.keywords" />
+  <vList
+    :listData="data.result"
+    :selected="data.selected"
+    :keywords="data.keywords"
+  />
 </template>
 
 <script>
 import { onMounted, watch, reactive } from 'vue'
 import axios from 'axios'
-import Clock from '@/components/clock.vue'
-import Input from './components/responsive_input.vue'
-import List from './components/list.vue'
+import vWeather from '@/components/weather.vue'
+import vClock from '@/components/clock.vue'
+import vInput from '@/components/searchBox.vue'
+import vList from '@/components/list.vue'
 export default {
   components: {
-    Clock,
-    Input,
-    List
+    vWeather,
+    vClock,
+    vInput,
+    vList
   },
   setup () {
     /**
@@ -65,7 +96,8 @@ export default {
      *  方法
      *
      */
-    const inputUpdateEvent = str => { // Input组件 - 内容更新事件
+    const inputUpdateEvent = (str) => {
+      // Input组件 - 内容更新事件
       data.keywords = String(str)
     }
 
@@ -74,39 +106,45 @@ export default {
      *  监听动态数据变化
      *
      */
-    watch(() => data.keywords, (newVal, oldVal) => {
-      if (!String(newVal).length) return
-      // 获取百度关键词联想数据
-      const params = {
-        params: {
-          ie: 'utf-8',
-          prod: 'pc',
-          from: 'pc_web',
-          wd: data.keywords
-        }
-      }
-      axios.get('https://www.baidu.com/sugrec', params).then(res => {
-        // 结果为空时
-        if (!Object.keys(res.data).length) {
-          data.result = []
-          return
-        }
-        // 反之
-        const tmp = []
-        if (res.data.g.length) {
-          for (let i = 0, len = res.data.g.length; i < len; i++) {
-            tmp.push({
-              text: res.data.g[i].q,
-              url: `https://www.baidu.com/s?ie=utf-8&wd=${res.data.g[i].q}`
-            })
+    watch(
+      () => data.keywords,
+      (newVal, oldVal) => {
+        if (!String(newVal).length) return
+        // 获取百度关键词联想数据
+        const params = {
+          params: {
+            ie: 'utf-8',
+            prod: 'pc',
+            from: 'pc_web',
+            wd: data.keywords
           }
         }
-        if (tmp.length) data.result = tmp
-        else data.result = []
-      }).catch(err => {
-        console.log(err)
-      })
-    })
+        axios
+          .get('https://www.baidu.com/sugrec', params)
+          .then((res) => {
+            // 结果为空时
+            if (!Object.keys(res.data).length) {
+              data.result = []
+              return
+            }
+            // 反之
+            const tmp = []
+            if (res.data.g.length) {
+              for (let i = 0, len = res.data.g.length; i < len; i++) {
+                tmp.push({
+                  text: res.data.g[i].q,
+                  url: `https://www.baidu.com/s?ie=utf-8&wd=${res.data.g[i].q}`
+                })
+              }
+            }
+            if (tmp.length) data.result = tmp
+            else data.result = []
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      }
+    )
 
     /**
      *
@@ -118,15 +156,23 @@ export default {
       document.addEventListener('keydown', (e) => {
         // 上箭头
         if (e.key === 'ArrowUp') {
-          data.selected = data.selected - 1 < 0 ? data.result.length - 1 < 0 ? 0 : data.result.length - 1 : data.selected - 1
+          data.selected =
+            data.selected - 1 < 0
+              ? data.result.length - 1 < 0
+                ? 0
+                : data.result.length - 1
+              : data.selected - 1
         }
         // 下箭头
         if (e.key === 'ArrowDown') {
-          data.selected = data.selected + 1 > data.result.length - 1 ? 0 : data.selected + 1
+          data.selected =
+            data.selected + 1 > data.result.length - 1 ? 0 : data.selected + 1
         }
         // 回车事件
         if (e.key === 'Enter') {
-          window.location.href = `https://www.baidu.com/s?ie=utf-8&wd=${data.result[data.selected].text}`
+          window.location.href = `https://www.baidu.com/s?ie=utf-8&wd=${
+            data.result[data.selected].text
+          }`
         }
         // Ctrl + F: 快速翻译
         if (e.ctrlKey && e.key === 'f') {
--- src/components/clock.vue
+++ src/components/clock.vue
@@ -15,8 +15,8 @@ export default {
      *
      */
     const data = reactive({
-      date: '0000-00-00',
-      time: '00:00:00'
+      date: '',
+      time: ''
     })
 
     /**
@@ -24,7 +24,8 @@ export default {
      *  静态数据
      *
      */
-    const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+    const week = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+    // const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
     /**
      *
@@ -52,23 +53,18 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-@font-face
-  font-family 'Share Tech Mono'
-  font-style normal
-  font-weight 400
-  src url('~@/assets/Share Tech Mono.woff2') format('woff2')
-  unicode-range U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD
-
 #vClock
-  font-family 'Share Tech Mono', monospace
+  font-family sans-serif
+  font-weight lighter
   display flex
   flex-flow column wrap
   text-align center
-  letter-spacing .2rem
   text-shadow 0 0 2px #000
   .time
-    font-size 3rem
+    font-size 2.5rem
     padding .4rem 0 0 0
+    letter-spacing .2rem
   .date
-    font-size 1rem
+    font-size .8rem
+    letter-spacing .1rem
 </style>
