d1221e2ecab341b7076c8023cfdbc61c77213c78
KiHanLee <s3238382606@163.com>
2021-12-28 17:14:09 +0800

    Modify time font and add weather component.

---
+++ src/components/weather.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="he-plugin-simple"></div>
+</template>
+
+<script>
+export default {
+  setup () {
+    window.WIDGET = {
+      CONFIG: {
+        modules: '01234',
+        background: '5',
+        tmpColor: 'FFFFFF',
+        tmpSize: '16',
+        cityColor: 'FFFFFF',
+        citySize: '16',
+        aqiColor: 'FFFFFF',
+        aqiSize: '16',
+        weatherIconSize: '24',
+        alertIconSize: '18',
+        shadow: '0',
+        language: 'auto',
+        fixed: 'true',
+        vertical: 'center',
+        horizontal: 'center',
+        right: '38',
+        top: '16',
+        key: '6b5ff096908947079b875900d57b5f48'
+      }
+    }
+    const tag = document.createElement('script')
+    tag.src = 'https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0'
+    document.querySelector('body').appendChild(tag)
+  }
+}
+</script>
+
+<style lang="stylus">
+#he-plugin-simple
+  z-index 1
+  #weather-view-he
+    padding .2rem
+    margin-top -.3rem
+    border-radius 1rem
+    #view-vertical
+      overflow scroll
+</style>
