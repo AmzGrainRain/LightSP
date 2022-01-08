e6c49ca2a9d0942fca1cfa58f16669d2558db6d5
KiHanLee <s3238382606@163.com>
2022-01-08 21:37:57 +0800

    Background use blur only in specific situations.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 背景组件 -->
-  <vBackground />
+  <vBackground :blur="cStatus.vBackground.blur" />
 
   <!-- 天气组件 -->
   <vWeather
@@ -71,6 +71,9 @@ export default {
       },
       vSettings: {
         show: false
+      },
+      vBackground: {
+        blur: false
       }
     })
     const data = reactive({
@@ -157,7 +160,11 @@ export default {
      *
      */
     watch(() => data.keywords, (newVal, oldVal) => {
-      if (!String(newVal).length) return
+      if (!String(newVal).length) {
+        cStatus.vBackground.blur = false
+        return
+      }
+      cStatus.vBackground.blur = true
       // 获取百度关键词联想数据
       axios.get('https://www.baidu.com/sugrec', {
         params: {
--- src/components/list.vue
+++ src/components/list.vue
@@ -2,14 +2,14 @@
   <div
     id="vList"
     class="m-t overflow-x-hide overflow-y-auto transition scrollbar-hide"
-    :class="{'show': listData.length && keywords.length}"
+    :class="{'vShow': listData.length && keywords.length}"
   >
     <ul class="m-tb-0 m-lr-auto w-90">
       <li class="m-b transition" v-for="(item, index) in listData" :key="index">
         <a
           class="d-inline-block p-lr-lg w-100 border-radius-lg transition blur"
           :href="item.url"
-          :class="{'selected': selected === index}"
+          :class="{'vSelected': selected === index}"
         >{{ item.text }}</a>
       </li>
     </ul>
@@ -37,9 +37,9 @@ a
   &:hover
     text-indent .5rem
     background-color #fffa
-.show
+.vShow
   height calc(100% - 12rem) !important
-.selected
+.vSelected
   text-indent .5rem !important
   background-color #fffa !important
 </style>
