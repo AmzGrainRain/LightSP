15ec2001cb7dc9ea0fa33d58b0a38b0b0726dae7
AmzGrainRain <amzgrainrain@outlook.com>
2024-05-10 14:24:01 +0800

    update README.md

--- README.md
+++ README.md
@@ -8,13 +8,15 @@
 
 ## 分发渠道
 
-[Mozila Firefox](https://addons.mozilla.org/zh-CN/firefox/addon/lightsp-%E8%BD%BB%E8%B5%B7%E5%A7%8B%E9%A1%B5)
+[Firefox](https://addons.mozilla.org/zh-CN/firefox/addon/lightsp-%E8%BD%BB%E8%B5%B7%E5%A7%8B%E9%A1%B5)
 
 [Microsoft Edge](https://microsoftedge.microsoft.com/addons/detail/mianbaepkeclkoclkoikhlgdfpdelnfi)
 
 ## 兼容性
 
-火狐浏览器、微软edge浏览器、360安全浏览器、360极速浏览器、Opera浏览器
+火狐浏览器、微软edge浏览器、360安全浏览器、360极速浏览器、Opera浏览器。
+
+360系浏览器、Opera浏览器请自行解压扩展导入使用。
 
 ## 初始化 & 启动
 
--- public/background.js
+++ public/background.js
@@ -1,12 +1,7 @@
-const redirectURLS = [
-    "opera://startpage/",
-    "browser://startpage/",
-    "chrome://startpage/",
-    "about://blank",
-    ""
-];
-
-chrome.tabs.onCreated.addListener(function (tab) {
-    if (!redirectURLS.includes(tab.url)) return;
-    chrome.tabs.update(tab.id, { url: "index.html" });
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+    if (reason === 'install') {
+        chrome.tabs.create({
+            url: "https://www.bing.com/search?q=edge浏览器如何启用扩展"
+        });
+    }
 });
